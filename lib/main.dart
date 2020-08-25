81353936fc4f93ca422a502ae8192ff02c8095bf
Naeem <naeem.alpha2labs1@outlook.com>
2020-08-25 11:38:23 +0500

    SI Calculator Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Simple Interest Calculator App",
     home: SIForm(),
+    theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent),
   ));
 }
 
@@ -16,40 +22,159 @@ class SIForm extends StatefulWidget {
 }
 
 class _SIFormState extends State<SIForm> {
-  var _currencies = {"Rupees", "Dollars", "Pounds"};
+  var _currencies = ["Rupees", "Dollars", "Pounds"];
   final _minimumPadding = 5.0;
+
+  var _currentItemSelected = '';
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+
+  var displayResult = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
 
   @override
   Widget build(BuildContext context) {
+    TextStyle textStyle = Theme
+        .of(context)
+        .textTheme
+        .title;
     return Scaffold(
-      appBar: AppBar(
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
         title: Text("Simple Interest Calculator App"),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            getImageAssets(),
-            Padding(
-                padding: EdgeInsets.only(
-                    top: _minimumPadding, bottom: _minimumPadding),
-                child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        labelText: 'Principal',
-                        hintText: 'Enter Principal e.g. 1200',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0))))),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'Rate Of Interest',
-                  hintText: 'In Percent',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0))),
-            ),
-          ],
-        ),
-      ),
+    ),
+    body: Form(
+    child:Padding(padding:EdgeInsets.all(_minimumPadding * 2),
+    child: ListView(
+    children: [
+    getImageAssets(),
+    Padding(
+    padding: EdgeInsets.only(
+    top: _minimumPadding, bottom: _minimumPadding),
+    child: TextField(
+    keyboardType: TextInputType.number,
+    style: textStyle,
+    controller: principalController,
+    decoration: InputDecoration(
+    labelText: 'Principal',
+    labelStyle: textStyle,
+    hintText: 'Enter Principal e.g. 1200',
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(5.0)),
+    ),
+    ),
+    ),
+    Padding(
+    padding: EdgeInsets.only(
+    top: _minimumPadding, bottom: _minimumPadding),
+    child: TextField(
+    style: textStyle,
+    controller: roiController,
+    keyboardType: TextInputType.number,
+    decoration: InputDecoration(
+    labelText: 'Rate Of Interest',
+    hintText: 'In Percent',
+    labelStyle: textStyle,
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(5.0)),
+    ),
+    ),
+    ),
+    Padding(
+    padding: EdgeInsets.only(
+    top: _minimumPadding, bottom: _minimumPadding),
+    child: Row(
+    children: [
+    Expanded(
+    child: TextField(
+    keyboardType: TextInputType.number,
+    style: textStyle,
+    controller: termController,
+    decoration: InputDecoration(
+    labelText: 'Term',
+    labelStyle: textStyle,
+    hintText: 'Time in years',
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(5.0),
+    ),
+    ),
+    ),
+    ),
+    Expanded(
+    child: DropdownButton<String>(
+    items: _currencies.map((String value) {
+    return DropdownMenuItem<String>(
+    value: value,
+    child: Text(value),
+    );
+    }).toList(),
+    value: _currentItemSelected,
+    onChanged: (String newValueSelected) {
+    //Code to execute when menu item is selected
+    _onDropdownItemSelected(newValueSelected);
+    },
+    ),
+    ),
+    ],
+    ),
+    ),
+    Padding(
+    padding: EdgeInsets.only(),
+    child: Row(
+    children: [
+    Expanded(
+    child: RaisedButton(
+    color: Theme.of(context).accentColor,
+    textColor: Theme.of(context).primaryColorDark,
+    child: Text(
+    'Calculate',
+    textScaleFactor: 1.5,
+    ),
+    onPressed: () {
+    setState(() {
+    this.displayResult = _calculateTotalReturns();
+    });
+    },
+    ),
+    ),
+    Expanded(
+    child: RaisedButton(
+    color: Theme.of(context).primaryColorDark,
+    textColor: Theme.of(context).primaryColorLight,
+    child: Text(
+    'Reset',
+    textScaleFactor: 1.5,
+    ),
+    onPressed: () {
+    setState(() {
+    _reset();
+    });
+    },
+    ),
+    ),
+    ],
+    ),
+    ),
+    Padding(
+    padding: EdgeInsets.all(_minimumPadding * 2),
+    child: Text(
+    this.displayResult,
+    style: textStyle,
+    ),
+    )
+    ],
+    )
+    ,
+    )
+    ,
     );
   }
 
@@ -63,5 +188,30 @@ class _SIFormState extends State<SIForm> {
         child: image,
       ),
     );
+  }
+
+  void _onDropdownItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentItemSelected = newValueSelected;
+    });
+  }
+
+  String _calculateTotalReturns() {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(principalController.text);
+    double term = double.parse(principalController.text);
+
+    double totalAmountPayable = principal + (principal * roi * term);
+    String result =
+        'After $term years,your investment will be worth $totalAmountPayable $_currentItemSelected';
+    return result;
+  }
+
+  void _reset() {
+    principalController.text = '';
+    roiController.text = '';
+    termController.text = '';
+    displayResult = '';
+    _currentItemSelected = _currencies[0];
   }
 }
